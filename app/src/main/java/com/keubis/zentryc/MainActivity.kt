168b66bd3c8426b9aa409712aaa80ade2de0e558
Keubis <kgarcmen@myuax.com>
2026-04-23 18:48:09 +0200

    feat: creacion de botones de navegacion con fragments y los layout

--- app/src/main/java/com/keubis/zentryc/MainActivity.kt
+++ app/src/main/java/com/keubis/zentryc/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.keubis.zentryc
 
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.keubis.zentryc.ui.base.BaseActivity
 import com.keubis.zentryc.ui.dashboard.DashboardFragment
 
@@ -10,8 +13,11 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            loadFragment(DashboardFragment())
-        }
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNav.setupWithNavController(navController)
     }
 }
